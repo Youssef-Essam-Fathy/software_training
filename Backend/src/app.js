16b324fb74ff9b0123e909b94a946c330777fb1d
abdoomr2003 <abdoomer1112003@gmail.com>
2025-08-07 20:31:14 +0300

    finish prepareing Backend ready for API

---
+++ Backend/src/app.js
@@ -0,0 +1,32 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db'); 
+const University = require('./models/university.model');
+
+
+
+connectDB();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/api', async (req, res) => {
+  try {
+    const universities = await University.find({});
+    res.json(universities);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch universities' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ Backend/src/models/university.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const universitySchema = new mongoose.Schema({
+  Index: Number,
+  '2026 Rank': Number,
+  '2025 Rank': Number,
+  Name: String,
+  Country: String,
+  Region: String,
+  Size: String,
+  Focus: String,
+  Research: String,
+  Status: String,
+  'AR SCORE': Number,
+  'AR RANK': mongoose.Schema.Types.Mixed,
+  'ER SCORE': Number,
+  'ER RANK': mongoose.Schema.Types.Mixed,
+  'FSR SCORE': Number,
+  'FSR RANK': mongoose.Schema.Types.Mixed,
+  'CPF SCORE': Number,
+  'CPF RANK': mongoose.Schema.Types.Mixed,
+  'IFR SCORE': Number,
+  'IFR RANK': mongoose.Schema.Types.Mixed,
+  'ISR SCORE': Number,
+  'ISR RANK': mongoose.Schema.Types.Mixed,
+  'ISD SCORE': Number,
+  'ISD RANK': mongoose.Schema.Types.Mixed,
+  'IRN SCORE': Number,
+  'IRN RANK': mongoose.Schema.Types.Mixed,
+  'EO SCORE': Number,
+  'EO RANK': mongoose.Schema.Types.Mixed,
+  'SUS SCORE': Number,
+  'SUS RANK': mongoose.Schema.Types.Mixed,
+  'Overall SCORE': Number,
+});
+
+const University = mongoose.model('University', universitySchema, 'egypt');
+module.exports = University; 
